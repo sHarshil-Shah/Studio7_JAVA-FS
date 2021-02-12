005a453d133dac6c74c706facfdc8953418646fe
HA20171511 <harshil.shah1@wipro.com>
2021-02-12 19:35:46 +0530

    Multiple class is working properly

--- Backend/Studio7/src/main/java/com/wipro/Studio7Application.java
+++ Backend/Studio7/src/main/java/com/wipro/Studio7Application.java
@@ -7,8 +7,12 @@ import com.wipro.repository.UserRepository;
 import java.util.stream.Stream;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.wipro")
 public class Studio7Application {
 
 	public static void main(String[] args) {
@@ -19,10 +23,20 @@ public class Studio7Application {
 	CommandLineRunner init(UserRepository userRepository) {
 		return args -> {
 			Stream.of("Admin").forEach(name -> {
-				User user = new User(name + "@mail.com", "pass", "India", true);
+				User user = new User(1L, name + "@mail.com", "pass", "India", true, "", "");
 				userRepository.save(user);
 			});
 			userRepository.findAll().forEach(System.out::println);
+		};
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
 		};
 	}
 }
--- Frontend/angularclient/src/app/model/user.ts
+++ Frontend/angularclient/src/app/model/user.ts
@@ -1,6 +1,9 @@
 export class User {
+    id: Number | undefined;
     email: string | undefined;
     pass: string | undefined;
     country: string | undefined;
     admin: boolean | undefined;
+    history: string | undefined;
+    watchList: string | undefined;
 }
