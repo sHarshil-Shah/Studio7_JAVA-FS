a1d47a71c0613a0617be510a07e060610ad928a6
Harshil Shah <35626302+sHarshil-Shah@users.noreply.github.com>
2023-05-14 00:24:36 -0300

    Update user.service.ts

--- Frontend/angularclient/src/app/service/user.service.ts
+++ Frontend/angularclient/src/app/service/user.service.ts
@@ -12,7 +12,7 @@ export class UserService {
   private usersUrl: string;
 
   constructor(private http: HttpClient) {
-    this.usersUrl = 'http://65.1.105.69:8080/users';
+    this.usersUrl = 'https://s7backend.onrender.com/users';
   }
 
   public findAll(): Observable<User[]> {
