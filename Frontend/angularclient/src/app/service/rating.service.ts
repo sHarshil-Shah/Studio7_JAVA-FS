34395270a7a062bb484813eb2016bcf9deb76915
Harshil Shah <35626302+sHarshil-Shah@users.noreply.github.com>
2023-05-14 00:24:11 -0300

    Update rating.service.ts

--- Frontend/angularclient/src/app/service/rating.service.ts
+++ Frontend/angularclient/src/app/service/rating.service.ts
@@ -12,7 +12,7 @@ export class RatingService {
   private ratingsUrl: string;
 
   constructor(private http: HttpClient) {
-    this.ratingsUrl = 'http://65.1.105.69:8080/ratings';
+    this.ratingsUrl = 'https://s7backend.onrender.com/ratings';
   }
 
 
